11d2e9bc0946f7456f7d695c5f3fb025b4e6eef4
riddhi-borad <riddhi.borad@yahoo.com>
2021-05-10 22:36:20 +0530

    commit

--- Routers/banner.js
+++ Routers/banner.js
@@ -20,7 +20,7 @@ new Banner(addBanner).save()
 router.get('/viewBanner',(req,res)=>{
     Banner.find({})
     .then((result)=>{
-        res.status(200).json(result)
+        res.status(200).json({result})
     })
     .catch((err)=>{
         console.log(err);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express=require('express');
 const app=express();
+app.use(express.static(__dirname+"/public"));
 var cors=require('cors');
 app.use(cors({credentials: true,methods: "GET, POST"}));
 
