29ffbae543bd09011fa139aab56a179d6ccfc13d
riddhi-borad <riddhi.borad@yahoo.com>
2021-05-10 19:04:03 +0530

    commit

---
+++ Routers/banner.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const Banner=require("./../models/banner");
+const upload=require('./../config/multer')
+router.post('/saveBanner',upload.single("image"),(req,res)=>{
+    let addBanner={
+    name:req.body.name,
+    image:req.file.filename,
+    visibility:true
+}
+new Banner(addBanner).save()
+    .then(()=>{
+        res.status(200).json({msg:"Banner image inserted successfully !"});
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+router.get('/viewBanner'),(req,res)=>{
+    Banner.find({result})
+    .then(()=>{
+        res.status(200).json({result})
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+}
+module.exports = router;
---
+++ models/banner.js
@@ -0,0 +1,20 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema
+var newschema=new mongoose.Schema({
+    name:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    },
+   visibility:{
+        type:Boolean,
+        required:true
+    },
+    
+   
+});
+module.exports=Banner=mongoose.model('banners',newschema)
