1ffd15b94597cb90d6bc462b23aef046802ba795
riddhi-borad <riddhi.borad@yahoo.com>
2021-05-22 21:03:30 +0530

    commit

--- config/multer.js
+++ config/multer.js
@@ -1,9 +1,10 @@
 const multer=require("multer");
-const DIR="./public";
+// const DIR="./public";
+const path = require('path');
 
 const storage=multer.diskStorage({
     destination:(req, res, cb)=>{
-        cb(null,"./public");
+        cb(null, path.join(__dirname, '/public/'));
     },
     filename:(req,file,cb)=>{
         const filename=file.originalname.toLowerCase().split(" ").join("-");
