5d290cc4e88c48891ce2425c3bca6e3303b7ee19
riddhi-borad <riddhi.borad@yahoo.com>
2021-05-22 19:54:01 +0530

    commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 
 const express=require('express');
 const app=express();
-// app.use(express.static(__dirname+"/public"));
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(express.static(__dirname+"/public"));
+// app.use('/static', express.static(path.join(__dirname, 'public')))
 var cors=require('cors');
 app.use(cors({credentials: true,methods: "GET, POST"}));
 
